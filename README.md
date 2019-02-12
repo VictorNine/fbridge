e1e6efd042246d4bca22a884319f439419baa5cd
VictorNine <victor99@protonmail.com>
2019-02-12 18:45:26 +0100

    first code commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # fbridge
 fbridge bridges facebook messenger with any service supported by matterbridge trough the API interface. 
 
-fbMatter is using [fbchat](https://github.com/carpedm20/fbchat/) to connect to facebook.
+fbridge is using [fbchat](https://github.com/carpedm20/fbchat/) to connect to facebook.
 
 ```
 Requirements:
@@ -9,7 +9,12 @@ $ pip install requests
 $ pip install fbchat
 $ pip install toml
 ```
-fbridge will send any message received to “dafaultgateway”. This needs to be in your matterbridge.toml file. You can then sort out the messages you want by added the threadID to config.toml
+Run the bridge:
+```
+$ python3 fbridge.py
+```
+
+fbridge will send any message received to “defaultgateway”. This needs to be in your matterbridge.toml file. You can then sort out the messages you want by added the threadID to config.toml
 
 Matterbridge needs to be restarted any time you want to reconnect this client. There is some issue causing messages to be dropped if not.
 
---
+++ fbridge.py
@@ -0,0 +1,104 @@
+import json
+import requests
+from threading import Thread
+import toml
+
+from fbchat import log, Client
+from fbchat.models import ThreadType, Message 
+
+users = dict()
+threads = dict()
+revThreads = dict() # Reverse lookup
+
+username = ""
+password = ""
+
+## Send message to matterBridge
+def sendMsg(username, gateway, text):
+    headers = {'content-type': 'application/json'}
+    payload = {"text": text,"username":username ,"gateway":gateway}
+    r = requests.post('http://localhost:4242/api/message', data=json.dumps(payload), headers=headers)
+
+
+# Subclass fbchat.Client and override required methods
+class FBListener(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        # Don't do anything with messages sendt by you
+        if author_id == self.uid:
+            return
+
+        self.markAsDelivered(thread_id, message_object.uid)
+#        self.markAsRead(thread_id)
+
+        log.info("{} fra: {} - from {} in {}".format(message_object, author_id, thread_id, thread_type.name))
+
+        username = ""
+        ## Find the username from author_id
+        if author_id in users:
+            username = users[author_id]
+        else:
+            username = author_id
+
+        ## Set a gateway from the thread_id
+        gateway = "defaultgateway"
+        if thread_id in threads:
+            gateway = threads[thread_id]
+
+        sendMsg(username, gateway, message_object.text)
+
+def listen(fbClient):
+        r = requests.get('http://localhost:4242/api/messages')
+        r = requests.get('http://localhost:4242/api/stream', stream=True)
+        for msg in r.iter_lines():
+            if msg:
+                print(msg)
+                jmsg = json.loads(msg)
+                if jmsg["gateway"] == "":
+                    continue
+                
+                if jmsg["gateway"] == "defaultgateway":
+                    sendMsg("bot", "defaultgateway", "This gateway is linked to every thread on facebook and can't be used for sending messages.") 
+                else:
+                    fbThread = revThreads[jmsg["gateway"]]
+                    if len(fbThread) > 10:
+                        threadType = ThreadType.GROUP
+                    else:
+                        threadType = ThreadType.USER
+
+                    fbClient.send(Message(text=jmsg["text"]), thread_id=fbThread, thread_type=threadType)
+        r.Close()
+
+
+def readConfig():
+    global username 
+    global password
+    global revThreads
+
+    ### Load config
+    f = open("config.toml", "r")
+    toml_string = f.read() 
+    parsed_toml = toml.loads(toml_string)
+   
+    userData = parsed_toml["login"]
+    username = userData["username"]
+    password = userData["password"]
+
+    th = parsed_toml["threads"]
+    us = parsed_toml["users"]
+
+    for key, value in th.items():
+        threads[key]=value["gateway"] 
+
+    for key, value in us.items():
+        users[key]=value["username"] 
+
+    revThreads = {v: k for k, v in threads.items()}
+
+readConfig()
+
+client = FBListener(username, password)
+
+t = Thread(target = listen, args=(client,))
+t.start()
+
+client.listen()
